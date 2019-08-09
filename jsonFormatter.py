6be3c99a89594512fd964cad881b8f6404243ea1
Anderson Mendes <anderson2000ano@gmail.com>
2019-08-09 21:05:03 +0900

    Automatic JSON Formatter
    Signed-off-by: Akito Mizukito <peak@echln.net>

---
+++ jsonFormatter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+#Originally made by andersonmendess and some more stuff added by peaktogoo.
+
+import os, json, sys
+
+paths = ['builds','devices.json']
+
+indents = 3
+
+def main():
+	if len(sys.argv) > 1:
+		formatter('builds/'+sys.argv[1]+".json")
+		exit()
+
+	for path in paths:
+		if os.path.isdir(path):
+			for file in os.listdir(path):
+				pathfile = f"{path}/{file}"
+				if "builds/example.json" == pathfile:
+					print("Not formatting example.json. [This is normal behavior]]")
+				else:
+					formatter(path+'/'+file)
+		elif os.path.isfile(path):
+  			formatter(path)
+
+
+def formatter(path):
+	if not path.endswith('.json'):
+		return
+	try:
+		data = json.loads(open(path).read())
+		outfile = open(path, 'w')
+		json.dump(data, outfile, indent=indents, ensure_ascii=False)
+		print(f"{path}, JSON Formatted!")
+	except Exception as e:
+		print(f"{path}, JSON Formatting errored with the following exception!\n{e}")
+
+if __name__== "__main__":
+  main()
