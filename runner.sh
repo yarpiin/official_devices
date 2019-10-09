77ea4b7ebf34d39dd6260de13d9bea150cd4b6cd
Akito Mizukito <peak@echln.net>
2019-10-10 02:03:59 +0900

    ci: sendAdmins is not needed afterall
    Signed-off-by: Akito Mizukito <peak@echln.net>

--- runner.sh
+++ runner.sh
@@ -9,10 +9,6 @@ COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
 COMMIT_SMALL_HASH="$(git rev-parse --short HEAD)"
 COMMIT_HASH="$(git rev-parse --verify HEAD)"
 
-function sendAdmins() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001176435322&parse_mode=Markdown"
-}
-
 function sendMaintainers() {
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001176435322&parse_mode=Markdown"
 }
@@ -21,12 +17,11 @@ printf "\n\n***Evolution X CI***\n\n"
 BUILD_START=$(date +"%s")
 
 if [ -n "$PULL_REQUEST_NUMBER" ]; then
-    sendAdmins "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
     sendMaintainers "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
 else
     git checkout master  > /dev/null
     git pull origin master  > /dev/null
-    sendAdmins "**I am building master branch job.** %0A**Commit Point:** [${COMMIT_SMALL_HASH}](https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH})"
+    sendMaintainers "**I am building master branch job.** %0A**Commit Point:** [${COMMIT_SMALL_HASH}](https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH})"
 fi
 
 node json_tester.js
@@ -39,13 +34,11 @@ if [ -n "$PULL_REQUEST_NUMBER" ]; then
 
     if [ "$RESULT" -eq 1 ]; then
         echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
-        sendAdmins "\`PR $PULL_REQUEST_NUMBER is failing checks. Please don't merge\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
         sendMaintainers "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\` %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
         exit 1
     else
         echo "Yay! My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.~"
-        sendAdmins "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
-        sendMaintainers "\`PR $PULL_REQUEST_NUMBER has passed all sanity checks. Please wait for the merge.\`"
+        sendMaintainers "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
         exit 0
     fi
 
@@ -54,7 +47,7 @@ fi
 if [ "$RESULT" -eq 1 ]; then
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
-    sendAdmins "\`Someone has merged a failing file. Please look in ASAP.\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
+    sendMaintainers "\`Someone has merged a failing file. Please look in ASAP.\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
     echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
     exit 1
 fi
@@ -69,7 +62,7 @@ if [[ ! "$COMMIT_MESSAGE" =~ "[EvolutionX-CI]" ]] && [ -n "$GIT_CHECK" ]; then
     git remote rm origin
     git remote add origin https://realakito:"${GH_PERSONAL_TOKEN}"@github.com/Evolution-X-Devices/official_devices.git
     git push -f origin master
-    sendAdmins "JSON Linted and Force Pushed!"
+    sendMaintainers "JSON Linted and Force Pushed!"
 fi
 BUILD_END=$(date +"%s")
 DIFF=$((BUILD_END - BUILD_START))
