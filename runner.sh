38196fcf35a9975db8f0e89d6c704cfa9c642ed0
Akito Mizukito <akito@evolution-x.org>
2022-04-16 22:23:22 +0900

    ci: Migrate to HTML
    Change-Id: Ib82e7eb2997f8f24f2a6e639764308e87d651e60
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- runner.sh
+++ runner.sh
@@ -8,18 +8,18 @@ COMMIT_SMALL_HASH="$(git rev-parse --short HEAD)"
 COMMIT_HASH="$(git rev-parse --verify HEAD)"
 
 function sendTG() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001539241326&parse_mode=Markdown"
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001539241326&parse_mode=HTML"
 }
 
 printf "\n\n***Evolution X CI***\n\n"
 BUILD_START=$(date +"%s")
 
 if [ -n "$PULL_REQUEST_NUMBER" ]; then
-    sendTG "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
+    sendTG "<code>Someone made a new pull request! Pull request number $PULL_REQUEST_NUMBER.</code>"
 else
     git checkout master  > /dev/null
     git pull origin master  > /dev/null
-    sendTG "**I am starting to build!** %0A**Commit Point:** [${COMMIT_SMALL_HASH}](https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH})"
+    sendTG "<b>CI is building...</b> %0A<b>Head:</b> <a href='https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH}'>${COMMIT_SMALL_HASH}</a>"
 fi
 
 node json_tester.js
@@ -29,17 +29,17 @@ RESULT=$?
 if [ -n "$PULL_REQUEST_NUMBER" ]; then
 
     if [ "$RESULT" -eq 1 ]; then
-        sendTG "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\` %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
+        sendTG "<code>Pull request $PULL_REQUEST_NUMBER is failing checks. The maintainer is requested to check it.</code> %0A%0A<b>Failed file(s):</b> <code>$(cat /tmp/failedfile)</code> %0A%0A<a href='https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER'>Pull request link</a>"
         exit 1
     else
-        sendTG "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
+        sendTG "<code>Pull request $PULL_REQUEST_NUMBER can be merged.</code> %0A%0A${ADMINS} %0A%0A<a href='https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER'>Pull request link</a>"
         exit 0
     fi
 
 fi
 
 if [ "$RESULT" -eq 1 ]; then
-    sendTG "\`Ay, Someone have merged broken file! Please take a look ASAP!\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
+    sendTG "<code>Retard Alert! Someone have merged broken file(s)! Please take a look ASAP!</code> %0A%0A${ADMINS} %0A%0A<b>Failed file(s):</b> <code>$(cat /tmp/failedfile)</code>"
     exit 1
 fi
 
@@ -53,6 +53,6 @@ if [[ ! "$COMMIT_MESSAGE" =~ "[EvolutionX-CI]" ]] && [ -n "$GIT_CHECK" ]; then
     git remote rm origin
     git remote add origin https://realakito:"${GH_PERSONAL_TOKEN}"@github.com/Evolution-X-Devices/official_devices.git
     git push -f origin master
-    sendTG "JSON Linted and Force Pushed!"
+    sendTG "CI have linted JSON, repository have been force pushed!"
 fi
 BUILD_END=$(date +"%s")
