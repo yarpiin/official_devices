1782d77969b033d8fe303f21c8b3619e3f050f8c
AnierinB <anierin@evolution-x.org>
2023-05-02 12:33:21 +0000

    [EvolutionX-CI]: generate_device_groups: Add some user input
    Dialogues asking whether to list:
        Corrected codenames and their corrections.
        Removed codenames.
        Final output.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- generate_device_groups.sh
+++ generate_device_groups.sh
@@ -4,11 +4,11 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 ENDCOLOR='\033[0m'
 
-# Exceptions for devices that don't have
+# Corrections for devices that don't have
 # groups that directly translate to their
 # codename.
-declare -A exceptions=(
-  # Google exceptions
+declare -A corrections=(
+  # Google corrections
   ["blueline"]="Pixel3"
   ["crosshatch"]="Pixel3"
   ["sargo"]="Pixel3"
@@ -18,7 +18,7 @@ declare -A exceptions=(
   ["bluejay"]="Pixel6"
   ["panther"]="Pixel7"
   ["cheetah"]="Pixel7"
-  # OnePlus exceptions
+  # OnePlus corrections
   ["fajita"]="OnePlus6"
   ["enchilada"]="OnePlus6"
   ["guacamoleb"]="OnePlus7"
@@ -31,40 +31,46 @@ declare -A exceptions=(
   ["lemonades"]="OnePlus8"
   ["lemonade"]="OnePlus9"
   ["lemonadep"]="OnePlus9"
-  # Realme exceptions
+  # Realme corrections
   ["bladerunner"]="_Bladerunner"
   ["bladerunner_48m"]="_Bladerunner"
-  # Samsung exceptions
+  # Samsung corrections
   ["crownlte"]="9810"
   ["starlte"]="9810"
   ["star2lte"]="9810"
-  # Xiaomi exceptions
+  # Xiaomi corrections
   ["raphael"]="Raphael_v2"
 )
 
-# Ignored devices if group isn't Evolution X
+# Remove devices if group isn't Evolution X
 # specific or if there is no group at all.
-declare -a ignored_devices=(
+declare -a remove_devices=(
   "taimen"
   "walleye"
   "redfin"
 )
 
 declare -A devices_by_brand
+corrected_devices=()
+removed_devices=()
 while IFS= read -r device; do
     brand=$(echo "$device" | jq -r '.brand')
     codename=$(echo "$device" | jq -r '.codename')
-    if [[ "${ignored_devices[*]}" =~ "$codename" ]]; then
+    if [[ "${remove_devices[*]}" =~ "$codename" ]]; then
+        removed_devices+=("$codename")
         continue
     fi
-    if [[ -n ${exceptions[$codename]} ]]; then
-        codename=${exceptions[$codename]}
+    if [[ -n ${corrections[$codename]} ]]; then
+        corrected_codename=${corrections[$codename]}
+        corrected_devices+=("$codename -> $corrected_codename")
+        codename="$corrected_codename"
     fi
     codename="${codename^}"
     if [[ ${devices_by_brand[$brand]} != *"${codename}"* ]]; then
         devices_by_brand[$brand]+="$codename "
     fi
 done < <(jq -c '.[]' devices.json)
+
 printf '%s\n' "${!devices_by_brand[@]}" | tr ' ' '\n' | sort -u | while read -r brand; do
     echo "${brand}:"
     for device in $(echo "${devices_by_brand[$brand]}" | tr ' ' '\n' | sort | tr '\n' ' '); do
@@ -72,6 +78,16 @@ printf '%s\n' "${!devices_by_brand[@]}" | tr ' ' '\n' | sort -u | while read -r 
     done
     echo ""
 done > ../device_groups.txt 2>&1
+
+corrected_devices_output="Corrected Devices:\n"
+for corrected_device in "${corrected_devices[@]}"; do
+  corrected_devices_output+="  - $corrected_device\n"
+done
+
+removed_devices_output="Removed Devices:\n"
+for removed_device in "${removed_devices[@]}"; do
+  removed_devices_output+="  - $removed_device\n"
+done
 
 echo -e "${GREEN}===========================================================${ENDCOLOR}"
 echo -e "${BLUE}      ______            __      __  _                _  __  ${ENDCOLOR}"
@@ -83,9 +99,28 @@ echo -e "${BLUE}                                                            ${EN
 echo -e "${BLUE}                         #KeepEvolving                      ${ENDCOLOR}"
 echo -e "${GREEN}Finished generating ../device_groups.txt                   ${ENDCOLOR}"
 echo -e "${GREEN}===========================================================${ENDCOLOR}"
-read -p "Would you like to display the output? (y/n) " choice
+corrected_output=""
+removed_output=""
+final_output=""
+while [[ ! $corrected_output =~ ^[yn]$ ]]; do
+    read -p "Would you like to view a list of corrected devices? (y/n) " corrected_output
+done
+while [[ ! $removed_output =~ ^[yn]$ ]]; do
+    read -p "Would you like to view a list of removed devices? (y/n) " removed_output
+done
+while [[ ! $final_output =~ ^[yn]$ ]]; do
+    read -p "Would you like to view the final device_groups.txt? (y/n) " final_output
+done
 echo -e "${GREEN}===========================================================${ENDCOLOR}"
-if [[ $choice == "y" ]]; then
+if [[ $corrected_output == "y" ]]; then
+  echo -e "${corrected_devices_output}"
+echo -e "${GREEN}===========================================================${ENDCOLOR}"
+fi
+if [[ $removed_output == "y" ]]; then
+  echo -e "${removed_devices_output}"
+echo -e "${GREEN}===========================================================${ENDCOLOR}"
+fi
+if [[ $final_output == "y" ]]; then
+  echo -e "Final output:"
   cat ../device_groups.txt
 fi
-echo -e "${ENDCOLOR}"
