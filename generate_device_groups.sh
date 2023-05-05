12c6ffdf0dd9724e69c4df554919e1cd59bd9425
AnierinB <anierin@evolution-x.org>
2023-05-05 14:32:45 -0700

    generate_device_groups: Add missing dependencies check
    - Checks if the required dependencies are available. If missing, it prompts
    the user for confirmation before proceeding to install the missing dependencies while
    providing a description of each dependency that will be installed. If the user declines,
    the script is halted.
    
    - Checks the availability of apt-get, pacman, and dnf using
    the command -v utility. Depending on which command is found, the script
    determines the corresponding Linux distribution (Debian/Ubuntu, Arch Linux,or Fedora)
    to proceed with.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- generate_device_groups.sh
+++ generate_device_groups.sh
@@ -1,8 +1,73 @@
 #!/bin/bash
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+ORANGE='\033[0;33m'
 ENDCOLOR='\033[0m'
+
+dependencies="jq coreutils"
+
+missing_dependencies=()
+for dependency in $dependencies; do
+  if ! dpkg -s "$dependency" >/dev/null 2>&1 && ! rpm -q "$dependency" >/dev/null 2>&1 && ! pacman -Q "$dependency" >/dev/null 2>&1; then
+    missing_dependencies+=("$dependency")
+  fi
+done
+
+if [ ${#missing_dependencies[@]} -ne 0 ]; then
+  echo -e "${ORANGE}Missing dependencies:${ENDCOLOR}"
+  for dependency in "${missing_dependencies[@]}"; do
+    case $dependency in
+      jq)
+        echo -e "$dependency: Lightweight and flexible command-line tool for parsing and manipulating JSON data."
+        ;;
+      coreutils)
+        echo -e "$dependency: Collection of essential command-line utilities for basic file and text manipulation tasks."
+        ;;
+    esac
+  done
+
+  while true; do
+    read -p "Do you want to install these dependencies? (y/n): " choice
+    case $choice in
+      [Yy]|[Yy][Ee][Ss])
+        if [ -x "$(command -v apt-get)" ]; then
+          echo -e "${ORANGE}Detected Debian/Ubuntu distribution, installing required dependencies...${ENDCOLOR}"
+          sudo apt-get update && sudo apt-get install -y "${missing_dependencies[@]}" || {
+            echo -e "${RED}Error: Failed to install required dependencies using apt-get.${ENDCOLOR}"
+            exit 1
+          }
+        elif [ -x "$(command -v pacman)" ]; then
+          echo -e "${ORANGE}Detected Arch Linux distribution, installing required dependencies...${ENDCOLOR}"
+          sudo pacman -Sy --noconfirm "${missing_dependencies[@]}" || {
+            echo -e "${RED}Error: Failed to install required dependencies using pacman.${ENDCOLOR}"
+            exit 1
+          }
+        elif [ -x "$(command -v dnf)" ]; then
+          echo -e "${ORANGE}Detected Fedora distribution, installing required dependencies...${ENDCOLOR}"
+          sudo dnf update -y && sudo dnf install -y "${missing_dependencies[@]}" || {
+            echo -e "${RED}Error: Failed to install required dependencies using dnf.${ENDCOLOR}"
+            exit 1
+          }
+        else
+          echo -e "${RED}Error: Unsupported distribution or package manager detected.${ENDCOLOR}"
+          exit 1
+        fi
+
+        echo -e "${GREEN}Dependencies successfully installed.${ENDCOLOR}"
+        break
+        ;;
+      [Nn]|[Nn][Oo])
+        echo -e "${RED}Missing dependencies not installed. Exiting!${ENDCOLOR}"
+        exit 0
+        ;;
+      *)
+        echo "Invalid choice. Please enter 'y' or 'n'."
+        ;;
+    esac
+  done
+fi
 
 devices=$(<devices.json)
 
@@ -40,10 +105,10 @@ echo -e "${BLUE}                         #KeepEvolving                      ${EN
 echo -e "${GREEN}Wrote device groups to ../device_groups.txt                ${ENDCOLOR}"
 echo -e "${GREEN}===========================================================${ENDCOLOR}"
 while true; do
-    read -p "How do you want to view the file? Choose an option: 
-    1. Cat 
-    2. Nano 
-    3. Vim 
+    read -p "How do you want to view the file? Choose an option:
+    1. Cat
+    2. Nano
+    3. Vim
     4. Gedit
     5. None of the above
     Enter your choice (1-5): " choice
@@ -66,7 +131,7 @@ while true; do
             break
             ;;
         5)
-            echo "No action taken."
+            echo -e "${ORANGE}No action taken.${ENDCOLOR}"
             break
             ;;
         *)
